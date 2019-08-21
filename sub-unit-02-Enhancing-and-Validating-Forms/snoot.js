ca38bfd6f2c0a2fc6a6b3165bc72f07ae1ce4fbe
Unknown <TAdcox009@west-mec.org>
2019-08-21 14:10:28 -0700

    commit 2
    Commit 2 for exercise 3, left off on part 3 step 10.

--- sub-unit-02-Enhancing-and-Validating-Forms/snoot.js
+++ sub-unit-02-Enhancing-and-Validating-Forms/snoot.js
@@ -8,9 +8,59 @@
 
 // Filename: snoot.js
 
+// Global variables
+var twentyNine = document.createDocumentFragment();
+var thirty = document.createDocumentFragment();
+var thirtyOne = document.createDocumentFragment();
+
+function setUpDays() {
+    var dates = document.getElementById("delivDy").getElementsByTagName("option");
+    twentyNine.appendChild(dates[28].cloneNode(true));
+    thirty.appendChild(dates[28].cloneNode(true));
+    thirty.appendChild(dates[29].cloneNode(true));
+    thirtyOne.appendChild(dates[28].cloneNode(true));
+    thirtyOne.appendChild(dates[29].cloneNode(true));
+    thirtyOne.appendChild(dates[30].cloneNode(true));
+}
+
+function updateDays() {
+    var deliveryDay =document.getElementById("delivDy");
+    var dates = deliveryDay.getElementsByTagName("option");
+    var deliveryMonth = document.getElementById("delivMo");
+    var deliveryYear = document.getElementById("delivYr");
+    var selectedMonth = deliveryMonth.options[deliveryMonth.selectedIndex].value;
+
+    while (dates[28]) {
+        deliveryDay.removeChild(dates[28]);
+    }
+
+    if (deliveryYear.selectedIndex === -1) {
+        deliveryYear.selectedIndex =0;
+    }
+
+    if (selectedMonth === "2" && deliveryYear.options[deliveryYear.selectedIndex].value ==="2020") {
+        deliveryDay.appendChild(twentyNine.cloneNode(true));
+    } else if (selectedMonth === "4" || selectedMonth === "6" ||selectedMonth === "9" || selectedMonth === "11") {
+        deliveryDay.appendChild(thirty.cloneNode(true));
+    } else if(selectedMonth === "1" || selectedMonth === "3" || selectedMonth === "5" || selectedMonth === "7" || selectedMonth === "8" || selectedMonth === "10" || selectedMonth === "12") {
+        deliveryDay.appendChild(thirtyOne.cloneNode(true));
+    }
+}
+
+/* run setUpPage() function when page finishes loading */
+if (window.addEventListener) {
+    window.addEventListener("load", setUpPage, false);
+} else if (window.attachEvent) {
+    window.attachEvent("onload", setUpPage);
+}
+
 function removeSelectDefaults() {
     var emptyBoxes = document.getElementsByTagName("select");
     alert("select lists: " + emptyBoxes.length)
+}
+
+function SetUpPAge() {
+    removeSelectDefaults();
 }
 
 function createEventListener() {
